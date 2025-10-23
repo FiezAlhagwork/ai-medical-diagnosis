b37fa3680cbb46be8c2d10fb1befa7f5c7a53e7c
fiezalhag <fiezalhag@gmail.com>
2025-10-23 18:14:14 +0300

    add function adminOnly

--- backend/Middleware/authMiddleware.js
+++ backend/Middleware/authMiddleware.js
@@ -25,4 +25,12 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-module.exports = { protect };
+const adminOnly = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    res.status(403).json({ message: "Admin access only" ,error:true});
+  }
+};
+
+module.exports = { protect , adminOnly };
