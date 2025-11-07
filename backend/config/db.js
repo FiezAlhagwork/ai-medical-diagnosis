50c064f631ca3aa4f091d27194f0516bab73317d
fiezalhag <fiezalhag@gmail.com>
2025-11-07 13:48:29 +0300

    Update MongoDB connection from global to local

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {});
+    await mongoose.connect(process.env.MONGO_URL_LOCAL, {});
     console.log(`MongoDB Connected`);
   } catch (error) {
     console.error(`❌ Error connecting to MongoDB: ${error.message}`);
