8594e53670287ce1cef0a938656b47041bbef486
fiezalhag <fiezalhag@gmail.com>
2026-01-05 16:27:18 +0300

    edit  response user  not found

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -111,7 +111,7 @@ const loginUser = async (req, res) => {
 const getUserProfile = async (req, res) => {
   try {
     const user = await User.findById(req.params.id).select("-password");
-    if (!user) return res.status(404).json({ message: "User not found" });
+    if (!user) return res.status(404).json({ message: "User not found" , error:true });
 
     if (
       req.user.role !== "admin" &&
